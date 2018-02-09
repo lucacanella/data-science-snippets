a203d0e29904b0e5cb4432756eaaffbd147ee3f3
Luca Canella - Diennea <luca.canella@diennea.com>
2018-02-09 12:44:49 +0100

    Adjusted comments.

--- open-refine-notes.py
+++ open-refine-notes.py
@@ -2,10 +2,11 @@
 # @author: Luca Canella
 #
 # Jython ref link: http://www.jython.org/docs/index.html
+#
 
 # Map: (Python/Jython)
-d = dict({ 1: "A", 2: "B", 3: "C", 4: "D" })
-return d[cells["type"]["value"]]
+d = dict({ 1: "A", 2: "B", 3: "C", 4: "D" }) #create a map (dictionary)
+return d[cells["type"]["value"]] #return mapped value
 
 # Regex: (Python/Jython)
 import re
@@ -14,15 +15,15 @@ return g
 
 # Regex 2: (Python/Jython)
 import re
-rege = ur'(<regex_group>)'
-va = row["cells"]["description"]["value"]
-g = re.search(rege, va)
-return g and len(g.group(1)) > 0 or 0
+rege = ur'(<regex_group>)' #regex string
+va = row["cells"]["description"]["value"] #get a column in this row
+g = re.search(rege, va) #search for regex in cell
+return g and len(g.group(1)) > 0 or 0 #return 1 if matched, hence group1 exists and is non empty, or return 0.
 
 # Json: (Python/Jython)
 import json
-obj = json.loads(value)
-return obj and obj["field"] or ""
+obj = json.loads(value) # parse json
+return obj and obj["field"] or "" #return single field or empty string
 
 # Datetime: (Python/Jython)
 from datetime import datetime #import datetime
